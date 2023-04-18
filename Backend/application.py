97437b0288d277e7893895c3eaa0ee635e812dbf
NILDARK <nilu.patel2002@gmail.com>
2023-04-18 19:34:52 +0530

    changes

--- Backend/application.py
+++ Backend/application.py
@@ -2,13 +2,40 @@ from flask import Flask,request,Response,json
 import pickle as pkl
 import pandas as pd
 import joblib
+import json
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 from scipy.sparse import hstack
 pickle_in = open("Classifiers.pkl","rb")
 classifiers=joblib.load(pickle_in)
 
+<<<<<<< Updated upstream
 pickle_in = open("CharVectorizer.pkl","rb") 
 char_vectorizer=joblib.load(pickle_in)
+=======
+# pickle_in = open("CharVectorizer.pkl","rb")
+# char_vectorizer=joblib.load(pickle_in)
+>>>>>>> Stashed changes
 
+def load_vectorizer(file_path):
+    """
+    Load a TfidfVectorizer object from a JSON file.
+    
+    Args:
+        file_path (str): The file path to load the JSON file.
+    
+    Returns:
+        TfidfVectorizer: The loaded TfidfVectorizer object.
+    """
+    # Read the parameters from the JSON file
+    with open(file_path, 'r') as f:
+        params = json.load(f)
+    vectorizer = TfidfVectorizer(vocabulary=params['vocabulary'])
+    # Set the idf_ attribute using the loaded values
+    vectorizer.idf_ = np.asarray(params['idf'])
+    return vectorizer
+char_vectorizer = load_vectorizer('CharVectorizer.json')
 pickle_in = open("WordVectorizer.pkl","rb")
 word_vectorizer=joblib.load(pickle_in)
 
@@ -32,5 +59,5 @@ def predict(text):
     return response
 
 if __name__=="__main__":
-    # application.run(host="127.0.0.1",port=5000,debug=True)
     application.run(debug=True)
+    # application.run(debug=True)
